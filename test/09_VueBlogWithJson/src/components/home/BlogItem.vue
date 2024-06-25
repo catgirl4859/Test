16f44e6a9043de08a044a10157c966e797f7ff72
minju132 <minju132123@gmail.com>
2024-06-25 15:58:57 +0900

    first

---
+++ test/09_VueBlogWithJson/src/components/home/BlogItem.vue
@@ -0,0 +1,38 @@
+<script setup>
+    import { useRouter } from "vue-router";
+
+    const router = useRouter();
+    const {blog} = defineProps(["blog"]);
+
+    function detailView(id) {
+        router.push(`/blogs/${id}`);
+    }
+</script>
+
+<template>
+    <div class="itemBox">
+        <label>제목 {{ blog.title }}</label>
+        <button @click="detailView(blog.id)">조회하기</button>
+    </div>
+</template>
+
+<style scoped>
+.itemBox {
+    display: flex;
+    justify-content: space-between;
+    border: 1px solid;
+    align-items: center;
+    height: 30px;
+    margin-bottom: 1%;
+    color: pink;
+}
+
+label {
+    margin-left: 1%;
+    color: green;
+}
+
+button {
+    margin-right: 1%;
+}
+</style>
